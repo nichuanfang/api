27bb8628e6c5cef398cfe1717af1588173d5441b
f18326186224 <f18326186224@gmail.com>
2023-11-30 03:17:18 +0800

    fix: 测试数据库功能

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 from flask import Flask
-import requests
-import os
+from flask import request
+from utils import github
+import json
+# import requests
+# import os
 
 app = Flask(__name__)
 
@@ -8,7 +11,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Hello World!'
-
 
 @app.route('/about')
 def about():
@@ -25,8 +27,25 @@ def contact():
     return 'Contact Page Route'
 
 
-@app.route('/api')
+@app.route('/api',methods=['GET'])
 def api():
     with open('data.json', mode='r') as my_file:
         text = my_file.read()
+        return text
+    
+# 增删改查demo
+@app.route('/api_add')
+def api_add():
+    # 触发github workflow
+    github.trigger_github_workflow('api_add', {'name': 'Jhon', 'age': 30})
+    # 读取json文件
+    with open('data.json', mode='r',encoding='utf-8') as my_file:
+        text = my_file.read()
+        # json转dict
+        data = json.loads(text)
+        # 增加数据
+        data.append({'name': 'Jhon', 'age': 30})
+        # dict转json
+        text = json.dumps(data)
+        github.trigger_github_workflow('api_add', {'name': 'Jhon', 'age': 30})
         return text
---
+++ utils/github.py
@@ -0,0 +1,22 @@
+import json
+import requests
+import os
+
+GH_TOKEN = os.environ.get('GITHUB_TOKEN')
+
+def trigger_github_workflow(event_type: str, client_payload: dict = {}):
+    """触发github workflow
+
+    Args:
+        repo (str): 仓库名
+        event_type (str): 事件类型
+        client_payload (dict): 负载
+    """
+    header = {
+        'Accept': 'application/vnd.github.everest-preview+json',
+        'Authorization': f'token {GH_TOKEN}'
+    }
+    data = json.dumps({"event_type": f"{event_type}",
+                       "client_payload": client_payload})
+    requests.post(f'https://api.github.com/repos/nichuanfang/api/dispatches',
+                  data=data, headers=header)
