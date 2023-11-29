632b413490941402d9615d0007d244313190a75a
f18326186224 <f18326186224@gmail.com>
2023-11-30 03:40:29 +0800

    fix: 测试更新数据

--- update_data.py
+++ update_data.py
@@ -1,3 +1,17 @@
 # 更新data.json文件
+import json
+import requests
+
+with open('data.json', mode='r+',encoding='utf-8') as my_file:
+    text = my_file.read()
+    data = json.loads(text)
+    # 新增数据
+    data['data'].append({
+        'name': '新数据',
+        'age': 18
+    })
+    # 写入文件
+    with open('data.json', mode='w+',encoding='utf-8') as my_file:
+        my_file.write(json.dumps(data))
 
 print('已更新数据!')
