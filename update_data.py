978bfcad233700d926e1a14fbb7b5a88cad84bb8
f18326186224 <f18326186224@gmail.com>
2023-11-30 03:44:25 +0800

    fix: 通过github更新数据

--- update_data.py
+++ update_data.py
@@ -2,16 +2,16 @@
 import json
 import requests
 
-with open('data.json', mode='r+',encoding='utf-8') as my_file:
-    text = my_file.read()
-    data = json.loads(text)
-    # 新增数据
-    data.append({
-        'name': '新数据',
-        'age': 18
-    })
-    # 写入文件
-    with open('data.json', mode='w+',encoding='utf-8') as my_file:
-        my_file.write(json.dumps(data))
+# with open('data.json', mode='r+',encoding='utf-8') as my_file:
+#     text = my_file.read()
+#     data = json.loads(text)
+#     # 新增数据
+#     data.append({
+#         'name': '新数据',
+#         'age': 18
+#     })
+#     # 写入文件
+#     with open('data.json', mode='w+',encoding='utf-8') as my_file:
+#         my_file.write(json.dumps(data))
 
 print('已更新数据!')
