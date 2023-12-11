e5d21a209c02bd2f24418408c323d65d18b12d3b
f18326186224 <f18326186224@gmail.com>
2023-12-11 14:57:39 +0800

    🦄 refactor: 准备重构flask组件

--- index.py
+++ index.py
@@ -4,8 +4,6 @@ from flask import request
 import json
 from db import turso
 import os
-# from utils import github
-# import requests
 
 # turso获取客户端(同步)
 client = turso.acquire_client_sync()
@@ -30,26 +28,3 @@ def portfolio():
 @app.route('/contact')
 def contact():
     return 'Contact Page Route'
-
-
-@app.route('/api', methods=['GET'])
-def api():
-    with open('data.json', mode='r') as my_file:
-        text = my_file.read()
-        return text
-
-
-@app.route('/turso', methods=['GET'])
-def turso_query():
-    """turso查询
-
-    Parameters:
-        sql: sql语句
-    """
-    client = turso.acquire_client_sync()
-    result = client.execute('SELECT * FROM frameworks')
-    # 转为str
-    res = []
-    for row in result:
-        res.append(row['url'])
-    return res
