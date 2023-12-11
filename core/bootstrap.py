3d8b744797a9b8b89daae98e826d77ac64c93a73
f18326186224 <f18326186224@gmail.com>
2023-12-11 16:40:58 +0800

    🐞 fix: Missing variable `handler` or `app` in file "index.py".

--- core/bootstrap.py
+++ core/bootstrap.py
@@ -4,10 +4,10 @@ from components import blueprints
 # 导出所有components下__init__.py的蓝图文件
 
 
-def init(app: Flask, host: str = '127.0.0.1', port: int = 5555, debug: bool = False):
+def init(app: Flask, debug: bool = False):
 
     # 挂载蓝图
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
     # 项目启动
-    app.run(host=host, port=port, debug=debug)
+    app.run(debug=debug)
