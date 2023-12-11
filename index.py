1557001ce048a3359e4490ffe00a4a599c4a33b5
f18326186224 <f18326186224@gmail.com>
2023-12-11 16:34:23 +0800

    🐞 fix: 启动崩溃

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from core import bootstrap
 
 if __name__ == '__main__':
     # 创建一个flask 应用
     app = Flask(__name__)
+    # 允许跨域
+    CORS(app)
     # 挂载路由 启动项目
     bootstrap.init(app=app)
