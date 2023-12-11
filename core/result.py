0accfaa98210893e9333f98eafaced032d2614b1
f18326186224 <f18326186224@gmail.com>
2023-12-11 17:36:16 +0800

    ✨ feat: 修复3,9兼容性问题

--- core/result.py
+++ core/result.py
@@ -1,12 +1,12 @@
 import json
-
+from typing import Union
 # 统一结果类
 
 
 class Result:
 
     @staticmethod
-    def success(data: str | dict | list | None = None, code: int = 200, msg: str = 'success') -> str:
+    def success(data: Union[str, dict, list, None] = None, code: int = 200, msg: str = 'success') -> str:
         res = {}
         res['data'] = data
         res['code'] = code
@@ -14,7 +14,7 @@ class Result:
         return json.dumps(res)
 
     @staticmethod
-    def fail(data: str | dict | list | None = None, code: int = 500, msg: str = 'failed') -> str:
+    def fail(data: Union[str, dict, list, None] = None, code: int = 500, msg: str = 'failed') -> str:
         res = {}
         res['data'] = data
         res['code'] = code
