fec1f1eb5dd864dd04b97cf2ceffa3860c299997
f18326186224 <f18326186224@gmail.com>
2023-12-11 16:29:33 +0800

    🦄 refactor: 通过蓝图组件的方式来注册路由

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from flask import Flask
 from core import bootstrap
 
 if __name__ == '__main__':
-    # 创建一个flask应用
+    # 创建一个flask 应用
     app = Flask(__name__)
     # 挂载路由 启动项目
     bootstrap.init(app=app)
